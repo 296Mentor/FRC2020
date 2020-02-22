d7122c5081b56f66f3187c0985a7bf44683e6265
robomentor <robomentor@ROBOTICS-04.lhs.loyola.ca>
2020-02-22 17:00:21 -0500

    Added a clock to countdown from 180 seconds using current time millisecond function.  This did however pause or not run the rest of the code.

--- Dashboard296-2019/src/ca/northernknights/frc2019/dashboard/Clock.java
+++ Dashboard296-2019/src/ca/northernknights/frc2019/dashboard/Clock.java
@@ -5,50 +5,43 @@ import java.awt.Label;
 import java.util.Date;
 
 public class Clock extends Label {
-
-	
-	
 	private long matchStartTime;
 	private boolean running;
-	
+
 	long DisplayTime = 3 * 60 * 1000;
 	int gamelenght = 3000;
-	
+	private long firstvalue;
+
 	public void start() {
 		this.running = true;
 		this.matchStartTime = System.currentTimeMillis();
-		
-		
+        this.firstvalue =  System.currentTimeMillis();
 	}
-	
+
 	public void stop() {
 		this.running = false;
 	}
-	
+
 	public void reset() {
 		this.running = false;
-		
+
 	}
-	
 
 	public Clock() {
 		this.setAlignment(CENTER);
 	}
 
-	
-	
-	
 	@Override
 	public void paint(Graphics g) {
+		this.firstvalue = System.currentTimeMillis();
+		while (true) {
+			
+			this.matchStartTime = System.currentTimeMillis();
+			
+			this.setText(Long.toString(1 * (Math.abs( 180 - (matchStartTime - firstvalue)/1000 ))));
 
-		
-
-		this.setText(Long.toString( 1 * (gamelenght - matchStartTime)));
-
-		super.paint(g);
+			super.paint(g);
+		}
 	}
-	
-		
+
 }
-
-
